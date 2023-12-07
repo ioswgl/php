c90b84490b46a0b65c4187d1375020dd7952acad
Luis Silva <ioswgl@gmail.com>
2023-12-07 15:44:07 -0400

    Modificaciones varias al código php del archivo segunda_pagina.php

--- segunda_pagina.php
+++ segunda_pagina.php
@@ -9,7 +9,12 @@
         <?php
             $nombre="Luis";
             $edad=38;
-            print "El nombre del usuario es $nombre";
+            print "El nombre del usuario es $nombre <br>"; // Imprime el valor de la variable
+            print 'El nombre del usuario es $nombre <br>'; // Imprime el nombre de la variable de forma literal
+            echo "El nombre es " . $nombre . " y tiene " . $edad . " años <br>"; // echo admite imprimir valores de variables o expresiones para que echo vaya imprimiendo
+            // print es una función que devuelve siempre el valor 1. Se toma más tiempo para imprimir en pantalla xq tiene que hacer más procesos internos.
+            // echo es una expresión
+            echo $nombre,$edad;
         ?>
     </body>
 </html>
