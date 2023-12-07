00f3b9f1ca716c71bd2e9e512cf0408af1ee593e
Luis Silva <ioswgl@gmail.com>
2023-12-07 16:24:01 -0400

    Se agregó comentario sobre el archivo flujo_ejecucion.php

--- flujo_ejecucion.php
+++ flujo_ejecucion.php
@@ -7,6 +7,15 @@
     </head>
     <body>
         <?php
+            /*
+                El Flujo de Ejecución de un programa en PHP 
+                se refiere a que el navegador web interpreta
+                las sentencias PHP de arriba hacia abajo, 
+                comenzando por todo lo que esté dentro del body 
+                y antes del bloque de código PHP para luego
+                seguir con las instrucciones que estén dentro 
+                del código PHP
+            */
             echo "Este es el primer mensaje <br>";
             echo "Este es el segundo mensaje <br>";
         ?>
